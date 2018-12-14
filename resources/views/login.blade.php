b4c88b72b8acc9876a7ec8d4e0dc9ad6e2565d98
masrayiemas <rayiemas.8b@gmail.com>
2018-12-14 22:06:23 +0700

    ganti tampilan login
    hehehehehehhehe oiya aku sambil install laravel mix, pake node js iseng wkw

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -30,17 +30,17 @@
 					<div class="left">
 						<div class="content">
 							<div class="header">
-								<div class="logo text-center"><img src="{{ URL('css/assets/img/logo-dark.png') }}" alt="Klorofil Logo"></div>
+								<div class="logo text-center"><img src="{{ URL('pict/abuw.png') }}" alt="Klorofil Logo"></div>
 								<p class="lead">Login to your account</p>
 							</div>
 							<form class="form-auth-small" action="index.php">
 								<div class="form-group">
 									<label for="signin-email" class="control-label sr-only">Email</label>
-									<input type="email" class="form-control" id="signin-email" value="samuel.gold@domain.com" placeholder="Email">
+									<input type="email" class="form-control" id="signin-email" value="" placeholder="Email">
 								</div>
 								<div class="form-group">
 									<label for="signin-password" class="control-label sr-only">Password</label>
-									<input type="password" class="form-control" id="signin-password" value="thisisthepassword" placeholder="Password">
+									<input type="password" class="form-control" id="signin-password" value="" placeholder="Password">
 								</div>
 								<div class="form-group clearfix">
 									<label class="fancy-checkbox element-left">
@@ -58,8 +58,8 @@
 					<div class="right">
 						<div class="overlay"></div>
 						<div class="content text">
-							<h1 class="heading">Free Bootstrap dashboard template</h1>
-							<p>by The Develovers</p>
+							<h1 class="heading">Helping You to Get India's Creatures.</h1>
+							<p>by MbulKringDevs</p>
 						</div>
 					</div>
 					<div class="clearfix"></div>
